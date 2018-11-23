b3ac3a7674bb277b28121f8cf2adb789af323ca7
Bren Briggs <code@fraq.io>
2018-11-23 10:11:09 -0500

    Fix tab width

--- hack.py
+++ hack.py
@@ -5,37 +5,38 @@ def read(url):
 	return requests.get(url)
 
 class Hacker:
-	def __init__(self, name, team):
-		self.name = name
-		self.team = team
-		self.motivation = 0
-		self.crying = True
+    def __init__(self, name, team):
+	self.name = name
+	self.team = team
+	self.motivation = 0
+	self.crying = True
 
-	def stop_crying(self):
-		self.crying = False
+    def stop_crying(self):
+	self.crying = False
 
-	def try_harder(self):
-		self.motivation += 11
+    def try_harder(self):
+	self.motivation += 11
 
-	def rtfm(self):
-		read("https://0x00sec.org/")
+    @staticmethod
+    def rtfm():
+	return read("https://0x00sec.org/")
 
-	def enumerate(self, target):
-		target.increase_progress(10)
-		return "Run gobuster & nmap?"
+    def enumerate(self, target):
+	target.increase_progress(10)
+	return "Run gobuster & nmap?"
 
 
 class Target:
-	def __init__(self, hostname):
-		self.hostname = hostname
-		self.hacked = False
-		self.hacked_progress = 0
+    def __init__(self, hostname):
+	self.hostname = hostname
+	self.hacked = False
+	self.hacked_progress = 0
 
-	def increase_progress(self, amount):
-		if self.hacked_progress >= 100:
-			self.hacked = True
-		else:
-			self.hacked_progress += amount
+    def increase_progress(self, amount):
+	if self.hacked_progress >= 100:
+            self.hacked = True
+	else:
+            self.hacked_progress += amount
 
 
 def hack(hacker, target):
@@ -51,7 +52,6 @@ def hack(hacker, target):
                 print("Profit!")
                 break
         time.sleep(0)
-
 
 target = Target("https://hackthebox.eu/")
 me = Hacker("pry0cc", "0x00sec")
