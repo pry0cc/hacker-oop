493b61f1c54b987d7ca47fd119fa256e91f9c5eb
pry0cc <pry0cc@protonmail.com>
2018-11-23 14:57:43 +0000

    added loads

--- hack.py
+++ hack.py
@@ -1,20 +1,55 @@
+import time
+import requests
+
+def read(url):
+	return requests.get(url)
+
 class Hacker:
 	def __init__(self, name, team):
 		self.name = name
 		self.team = team
+		self.motivation = 0
+		self.crying = True
 
-def hack(hacker, host):
+	def stop_crying(self):
+		self.crying = False
+
+	def try_harder(self):
+		self.motivation += 11
+
+	def rtfm(self):
+		read("https://0x00sec.org/")
+
+	def enumerate(self, target):
+		target.increase_progress(10)
+		return "Run gobuster & nmap?"
+
+
+class Target:
+	def __init__(self, hostname):
+		self.hostname = hostname
+		self.hacked = False
+		self.hacked_progress = 0
+
+	def increase_progress(amount):
+		if self.hacked_progress >= 100:
+			self.hacked = True
+		else:
+			self.hacked_progress += amount
+
+
+def hack(hacker, target):
     while True:
         if hacker.motivation == 0:
             hacker.stop_crying()
             hacker.try_harder()
             hacker.rtfm()
         else:
-            hacker.enumerate(host)
+            hacker.enumerate(target)
             if host.hacked == True:
                 print("???")
                 print("Profit!")
-        time.sleep 0
+        time.sleep(0)
 
 me = Hacker("pry0cc", "0x00sec")
 hack(me, "https://hackthebox.eu")
